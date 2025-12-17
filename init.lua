205c088d383b14bf86c96d8a385833cfeb0016ec
conditionull <twilight_forest@icloud.com>
2025-12-17 17:51:53 -0500

    feat: hover plugin setup

--- init.lua
+++ init.lua
@@ -8,13 +8,23 @@ vim.opt.shiftwidth = 2
 vim.opt.linebreak = true
 vim.opt.breakindent = true -- wrapped lines maintain proper indentation
 vim.opt.showbreak = "↪ "
+vim.opt.guicursor = "n:block,v:ver25,i:ver25,r:hor20,o:hor50"
 -- Make 'j' and 'k' move by visual lines when wrapped
 vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
 vim.keymap.set('n', '<Down>', 'gj', { noremap = true, silent = true })
 vim.keymap.set('n', '<Up>', 'gk', { noremap = true, silent = true })
+
+vim.opt.clipboard = "unnamedplus"
 vim.keymap.set("n", "dd", '"_dd', { noremap = true })
 vim.keymap.set("v", "d", '"_d', { noremap = true })
+vim.keymap.set({ "n", "v" }, "d", '"_d', { noremap = true })
+vim.keymap.set({ "n", "v" }, "D", '"_D', { noremap = true })
+vim.keymap.set({ "n", "v" }, "x", '"_x', { noremap = true })
+vim.keymap.set({ "n", "v" }, "X", '"_X', { noremap = true })
+vim.keymap.set({ "n", "v" }, "c", '"_c', { noremap = true })
+vim.keymap.set({ "n", "v" }, "C", '"_C', { noremap = true })
+
 -- shift up/down 4 spaces
 vim.keymap.set('n', '<S-Down>', '4j', { noremap = true, silent = true })
 vim.keymap.set('n', '<S-Up>', '4k', { noremap = true, silent = true })
@@ -28,9 +38,6 @@ vim.keymap.set("v", "<A-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<A-Up>",   ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<A-Down>", ":m .+1<CR>==")
 vim.keymap.set("n", "<A-Up>",   ":m .-2<CR>==")
-
-vim.opt.clipboard = "unnamedplus"
-vim.opt.guicursor = "n:block,v:ver25,i:ver25,r:hor20,o:hor50"
 
 -- Save with 'gs' because i don't like pressing ':'
 vim.keymap.set("n", "gs", function()
@@ -56,7 +63,7 @@ Plug 'stevearc/conform.nvim'
 Plug 'mason-org/mason.nvim'
 Plug 'mason-org/mason-lspconfig.nvim'
 Plug 'windwp/nvim-autopairs'
-
+Plug 'lewis6991/hover.nvim'
 Plug 'hrsh7th/nvim-cmp'
 Plug 'hrsh7th/cmp-nvim-lsp'
 Plug 'hrsh7th/cmp-buffer'
@@ -116,6 +123,60 @@ require("mason-lspconfig").setup({
     ensure_installed = { "pyright", "lua_ls", "ts_ls", "gopls", "clangd" },
     automatic_enable = true,
 })
+
+-- Hover (floating window)
+require('hover').config({
+  --- List of modules names to load as providers.
+  --- @type (string|Hover.Config.Provider)[]
+  providers = {
+    'hover.providers.diagnostic',
+    'hover.providers.lsp',
+    'hover.providers.dap',
+    'hover.providers.man',
+    'hover.providers.dictionary',
+    -- Optional, disabled by default:
+    -- 'hover.providers.gh',
+    -- 'hover.providers.gh_user',
+    -- 'hover.providers.jira',
+    -- 'hover.providers.fold_preview',
+    -- 'hover.providers.highlight',
+  },
+  preview_opts = {
+    border = 'rounded'
+  },
+  -- Whether the contents of a currently open hover window should be moved
+  -- to a :h preview-window when pressing the hover keymap.
+  preview_window = false,
+  title = true,
+  mouse_providers = {
+    'hover.providers.lsp',
+  },
+  mouse_delay = 800
+})
+
+-- Setup keymaps
+vim.keymap.set('n', 'K', function()
+  require('hover').open()
+end, { desc = 'hover.nvim (open)' })
+
+vim.keymap.set('n', 'gK', function()
+  require('hover').enter()
+end, { desc = 'hover.nvim (enter)' })
+
+vim.keymap.set('n', '<C-p>', function()
+    require('hover').switch('previous')
+end, { desc = 'hover.nvim (previous source)' })
+
+vim.keymap.set('n', '<C-n>', function()
+    require('hover').switch('next')
+end, { desc = 'hover.nvim (next source)' })
+
+-- Mouse support
+vim.keymap.set('n', '<MouseMove>', function()
+  require('hover').mouse()
+end, { desc = 'hover.nvim (mouse)' })
+
+vim.o.mousemoveevent = true
 
 -- nvim-cmp setup
 local cmp = require'cmp'
